c7ca92626b4af7db867b617054ec68c71d5deb24
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 22:00:00 +0700

    Define instance methods

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ class Person:
     self.name = name
     self.age = age
 
+  def greet(self):
+    return f"Hi, It's {self.name}"
+
 if __name__ == "__main__":
   # Define a class
 
@@ -14,6 +17,11 @@ if __name__ == "__main__":
   # person.name = "John"
   # print(person.name)
 
+  # person = Person("John", 25)
+  # print(person.name)
+  # print(person.age)
+
+  # Define instance methods
+
   person = Person("John", 25)
-  print(person.name)
-  print(person.age)
+  print(person.greet())
