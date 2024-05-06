6399787a1395af40af8fe4762b008736dea6b43d
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 21:58:29 +0700

    Define instance attributes

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 class Person:
-  pass
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
 if __name__ == "__main__":
   # Define a class
 
-  person = Person()
-  print(type(person))
+  # person = Person()
+  # print(type(person))
+
+  # Define instance attributes
+
+  # person.name = "John"
+  # print(person.name)
+
+  person = Person("John", 25)
+  print(person.name)
+  print(person.age)
